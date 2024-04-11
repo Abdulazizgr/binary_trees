e8215590e7d47d5ddcee52fe4005d6b68fcbb315
Raji Nurudeen <orizon.bot21@gmail.com>
2024-04-11 21:01:42 +0100

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_depth - Measures the depth of a node in the binary tree
  * @tree: Pointer to the node used to measure the depth
- * 
+ *
  * Return: the depth of the node.
  */
 
@@ -14,6 +14,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree->parent != NULL)
 		return (1 + binary_tree_depth(tree->parent));
-	
+
 	return (0);
 }

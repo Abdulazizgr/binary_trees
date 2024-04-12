2b49d75b6bf35dc5b14b784a791ff287d60f8062
Raji Nurudeen <orizon.bot21@gmail.com>
2024-04-12 03:35:29 +0100

    16. Is perfect modified

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,18 +12,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int right_height, left_height;
 
 	if (tree == NULL)
-		return (0);
+		return (1);
 
 	left_height = height(tree->left);
 	right_height = height(tree->right);
 
 	if (left_height != right_height)
 		return (0);
-	else
-		return (1);
 
 	return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
+
+	return (1);
 }
 
 /**
@@ -35,7 +35,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 int height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	else
 		return (1 + max(height(tree->left), height(tree->right)));
 }
